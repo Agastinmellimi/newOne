601ef291ea4901d6b6cd9fa7072327623298413e
Agastinmellimi <125130023+Agastinmellimi@users.noreply.github.com>
2024-02-18 17:54:50 +0530

    updated

--- index.js
+++ index.js
@@ -12,14 +12,14 @@ app.use(express.json());
 app.use(cors())
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, HEAD, OPTIONS, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Origin, X-Requsted-With, Accept, Authorization');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization')
   next();
 });
 
 
 
-const dbPath = path.join(__dirname, "userData.db");
+const dbPath = path.join(__dirname, './userData.db');
 let db = null;
 
 const intializeDbAndServer = async () => {
